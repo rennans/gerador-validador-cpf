0ebc0c7e5ff1933358ad405e990a4b54378ea0bf
Rennan dos Santos <rennan-ssantos@hotmail.com>
2025-01-15 21:41:41 -0300

    arrumação do código e identação, commit final

--- validador-cpf.py
+++ validador-cpf.py
@@ -29,7 +29,6 @@ for digito in nove_digitos:
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 
-
 dez_digitos = cpf_formatado[:10]
 contador_regressivo_2 = 11
 resultado_digito_2 = 0
@@ -40,7 +39,6 @@ for digito in dez_digitos:
 
 digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
-
 
 ultimos_dois_digitos = cpf_formatado[-2:]
 
